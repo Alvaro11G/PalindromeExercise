4570c94b37bb8197c38551079e7d59324fc8c865
Alvaro11G <agarcia11746@gmail.com>
2022-10-24 21:23:30 -0600

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string str)
+        {
+            //reverse word
+            var reversed = "";
+            str = str.ToLower();
+            for(int i = str.Length -1; i >= 0; i--)
+            {
+                reversed += str[i];
+            }
+            //compare the original with reversed
+            if(reversed == str)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
